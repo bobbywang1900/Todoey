671c27f6665b4b93155b0d658a92e1cdc8c5975c
Irving Gonzalez <irvinggon3@yahoo.com>
2018-03-27 20:33:30 -0400

    Update this lol

---
+++ Todoey/Controllers/CategoryViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by Irving Gonzalez on 3/27/18.
+//  Copyright © 2018 Crypto Royals. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+    
+    //Mark: - TableView Datasource Methods
+    
+    //Mark: - Data Manipulation Methods
+    
+    //Mark: - Add New Categories
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+    }
+
+    //Mark: - Tableview Delegate Methods
+}
